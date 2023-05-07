2a044c99d6afd1ec9349e48e7363475e069dc4ef
Santhu2022 <mogilipakasantosh@gmail.com>
2023-05-07 10:18:49 +0000

    Login Component Completed

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import {Switch, Route} from 'react-router-dom'
+import Login from './components/Login'
+import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+// These are the lists used in the application. You can move them to any component needed.
+const employmentTypesList = [
+  {
+    label: 'Full Time',
+    employmentTypeId: 'FULLTIME',
+  },
+  {
+    label: 'Part Time',
+    employmentTypeId: 'PARTTIME',
+  },
+  {
+    label: 'Freelance',
+    employmentTypeId: 'FREELANCE',
+  },
+  {
+    label: 'Internship',
+    employmentTypeId: 'INTERNSHIP',
+  },
+]
+
+const salaryRangesList = [
+  {
+    salaryRangeId: '1000000',
+    label: '10 LPA and above',
+  },
+  {
+    salaryRangeId: '2000000',
+    label: '20 LPA and above',
+  },
+  {
+    salaryRangeId: '3000000',
+    label: '30 LPA and above',
+  },
+  {
+    salaryRangeId: '4000000',
+    label: '40 LPA and above',
+  },
+]
+
+// Replace your code here
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={Login} />
+    <ProtectedRoute exact path="/" component={Home} />
+  </Switch>
+)
+
+export default App
