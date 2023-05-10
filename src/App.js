27a4679537b8aa0f90cd216286c2c734a195c858
Santhu2022 <mogilipakasantosh@gmail.com>
2023-05-10 12:22:45 +0000

    NotFound Route Implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
 import ProtectedRoute from './components/ProtectedRoute'
+import NotFound from './components/NotFound'
 
 import './App.css'
 
@@ -12,6 +13,8 @@ const App = () => (
     <Route exact path="/login" component={Login} />
     <ProtectedRoute exact path="/" component={Home} />
     <ProtectedRoute exact path="/jobs" component={Jobs} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
   </Switch>
 )
 
