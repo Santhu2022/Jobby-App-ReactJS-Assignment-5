ee31e83f39d9efbdabc5793e034fc04f09ceffc1
Santhu2022 <mogilipakasantosh@gmail.com>
2023-05-11 08:53:44 +0000

    JobItemDetails Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import Jobs from './components/Jobs'
 import ProtectedRoute from './components/ProtectedRoute'
 import NotFound from './components/NotFound'
+import JobItemDetails from './components/JobItemDetails'
 
 import './App.css'
 
@@ -13,6 +14,7 @@ const App = () => (
     <Route exact path="/login" component={Login} />
     <ProtectedRoute exact path="/" component={Home} />
     <ProtectedRoute exact path="/jobs" component={Jobs} />
+    <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
     <Route exact path="/not-found" component={NotFound} />
     <Redirect to="/not-found" />
   </Switch>
